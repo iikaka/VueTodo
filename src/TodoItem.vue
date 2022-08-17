55c4bc18d2e7c11a2e7d6d72063eea3a1756347f
iikaka <13180599752@163.com>
2022-08-17 15:11:52 +0800

    双击进入编辑状态 自动获得焦点 回车、失去焦点提交编辑 esc取消编辑

--- src/TodoItem.vue
+++ src/TodoItem.vue
@@ -1,11 +1,11 @@
 <template>
-   <li :class="{completed:todo.hasCompleted}">
+   <li :class="{completed:todo.hasCompleted,editing:editedTodo==todo}">
      <div class="view">
        <input type="checkbox" class="toggle" v-model="check">
-       <label>{{todo.value}}</label>
+       <label @dblclick="editTodo">{{todo.value}}</label>
        <button class="destroy" @click="delTodo"></button>
      </div>
-     <input type="text" class="edit">
+     <input type="text" class="edit" :value="todo.value" v-focus @keyup.enter="doneTodo" @blur="doneTodo" @keyup.esc="cancelTodo" >
    </li>
 </template>
 
@@ -14,12 +14,33 @@
     export default {
         name:"TodoItem",
         props:['todo'],
+        data(){
+            return{
+                editedTodo:""
+            }
+        },
         methods: {
             delTodo(){
                 this.$parent.todoDatas=this.$parent.todoDatas.filter(value=>{
                     return !(value.id==this.todo.id)
                 })
+            },
+            editTodo(){
+                this.editedTodo=this.todo;
+            },
+            doneTodo(e){
+                this.$parent.todoDatas=this.$parent.todoDatas.map(v=>{
+                    if(v.id==this.todo.id){
+                        v.value=e.target.value.trim();
+                    }
+                    return v;
+                })
+                this.editedTodo=''
+            },
+            cancelTodo(){
+                this.editedTodo=''
             }
+
         },
         computed:{
             check:{
@@ -34,6 +55,11 @@
                         return v
                     })
                 }
+            }
+        },
+        directives:{
+            focus(elm){
+                elm.focus()
             }
         }
     }
