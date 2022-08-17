4ae05c166fd3c55586f984efc43066a2bf131cc7
iikaka <13180599752@163.com>
2022-08-17 14:59:43 +0800

    全选或全不选

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,8 @@
       <input type="text" class="new-todo" v-model="newTodo" @keyup.enter="addTodo">
     </header>
     <section class="main">
-        <input type="checkbox" id="toggle-all" class="toggle-all">
-        <label for="toggle-all"></label>
+         <input type="checkbox" id="toggle-all" class="toggle-all" v-model="isAll">
+        <label for="toggle-all" ></label>
         <ul class="todo-list">
              <todo-item v-for="todo in todoDatas" :key="todo.id" :todo="todo"></todo-item>
         </ul>
@@ -18,7 +18,7 @@
 <script>
 import TodoItem from '@/TodoItem.vue';
 import TodoFooter from '@/TodoFooter.vue'
-  export default {
+export default {
     name:'App',
     components:{TodoItem,TodoFooter},
     data(){
@@ -36,8 +36,22 @@ import TodoFooter from '@/TodoFooter.vue'
         todo.hasCompleted=false;
         this.todoDatas.push(todo);
         this.newTodo=''
-      },
-    }
+      }
+    },
+      computed:{
+        isAll:{
+          get(){
+            return false;
+          },
+          set(value){
+            this.todoDatas.map(todo=>{
+              todo.hasCompleted=value;
+              return value;
+            })
+          }
+        }
+      }
+
   }
 </script>
 
